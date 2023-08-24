e924ec283f24fbe2606b0feade91058b229fa6f7
ColinMcHugh <colinmchugh87@gmail.com>
2023-08-24 12:46:21 -0400

    fixing maps

--- client/src/components/Posts/Post/map.js
+++ client/src/components/Posts/Post/map.js
@@ -1,32 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function MapApp() {
-  if (!("geolocation" in navigator)) {
-    console.error("Geolocation is not supported by this browser.");
-  } else {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const lat = Number(position.coords.latitude);
-        const long = Number(position.coords.longitude);
-        const apiKey = "7507ea0c02084a469414d51670a73f72";
-        console.log(`Latitude: ${lat}, longitude: ${long}`);
+  const [city, setCity] = useState(""); // Initialize city state
 
-        fetch(
-          `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&type=city&lang=en&limit=1&format=json&apiKey=${apiKey}`
-        )
-          .then((response) => response.json())
-          .then((result) => {
-            const city = result.results[0].address_line1;
-            console.log(city);
-            return result;
-          })
-          .catch((error) => console.log("error", error));
-      },
-      (error) => {
-        console.error("Error getting user location:", error);
-      }
-    );
-  }
-  return <h3>city here</h3>;
+  useEffect(() => {
+    if (!("geolocation" in navigator)) {
+      console.error("Geolocation is not supported by this browser.");
+    } else {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = Number(position.coords.latitude);
+          const long = Number(position.coords.longitude);
+          const apiKey = "7507ea0c02084a469414d51670a73f72";
+
+          fetch(
+            `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&type=city&lang=en&limit=1&format=json&apiKey=${apiKey}`
+          )
+            .then((response) => response.json())
+            .then((result) => {
+              const fetchedCity = result.results[0].address_line1;
+              setCity(fetchedCity);
+            })
+            .catch((error) => console.log("error", error));
+        },
+        (error) => {
+          console.error("Error getting user location:", error);
+        }
+      );
+    }
+  }, []);
+
+  return <h3>{city ? `City: ${city}` : "Loading city..."}</h3>;
 }
+
 export default MapApp;
