31bbd9a0c48a08ab2b05107d278ca4dfed85892a
bigloumeanie <louielacarbonara@Gmail.com>
2023-08-24 11:54:39 -0400

    mapapp integration

--- client/src/components/Posts/Post/map.js
+++ client/src/components/Posts/Post/map.js
@@ -1,57 +1,34 @@
 import React, { useState, useEffect } from "react";
 
 function MapApp() {
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
-  const [locationInfo, setLocationInfo] = useState(null);
+  if (!("geolocation" in navigator)) {
+    console.error("Geolocation is not supported by this browser.");
+  } else {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = Number(position.coords.latitude);
+        const long = Number(position.coords.longitude);
+        const apiKey = "7507ea0c02084a469414d51670a73f72";
+        console.log(`Latitude: ${lat}, longitude: ${long}`);
+        console.log(position);
+        console.log(typeof lat);
 
-  useEffect(() => {
-    if ("geolocation" in navigator) {
-      const watchId = navigator.geolocation.watchPosition(
-        function (position) {
-          const newLat = position.coords.latitude;
-          const newLng = position.coords.longitude;
-
-          console.log(`Latitude: ${newLat}, longitude: ${newLng}`);
-          console.log(position);
-
-          setLat(newLat);
-          setLng(newLng);
-
-          // Fetch location information using Nominatim reverse geocoding
-          fetch(
-            `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${newLat}&lon=${newLng}`
-          )
-            .then((response) => response.json())
-            .then((data) => {
-              const state = data.address.state || "Unknown State";
-              setLocationInfo(`${state}`);
-            })
-            .catch((error) => {
-              console.error("Error fetching location data:", error);
-            });
-        },
-        function (error) {
-          console.error("Error getting user location:", error);
-        }
-      );
-
-      // Clean up the geolocation watch when the component unmounts
-      return () => {
-        navigator.geolocation.clearWatch(watchId);
-      };
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-    }
-  }, []); // Empty dependency array to run the effect only once
-
-  return (
-    <div>
-      {lat !== null && lng !== null && (
-        <div>{locationInfo && <p> Location: {locationInfo}</p>}</div>
-      )}
-    </div>
-  );
+        fetch(
+          `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&type=city&lang=en&limit=1&format=json&apiKey=${apiKey}`
+        )
+          .then((response) => response.json())
+          .then((result) => {
+            const city = result.results[0].address_line1;
+            console.log(city);
+            return city;
+          })
+          .catch((error) => console.log("error", error));
+      },
+      (error) => {
+        console.error("Error getting user location:", error);
+      }
+    );
+  }
 }
 
 export default MapApp;
