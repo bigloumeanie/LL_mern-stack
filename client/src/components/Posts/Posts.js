c3e86d55da0b31413fe9302833a039dfa1dee044
cw-roy <curtis_roy@msn.com>
2023-08-30 08:06:11 -0700

    Modify sort order

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -9,10 +9,20 @@ const Posts = ({ setCurrentId }) => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
 
+    // Sort the posts array in descending order by createdAt
+    const sortedPosts = [...posts].sort((a, b) => {
+        const dateA = new Date(a.createdAt).getTime();
+        const dateB = new Date(b.createdAt).getTime();
+        console.log(dateA);
+        console.log(dateB);
+        console.log(dateB - dateA);
+        return dateB - dateA;
+    });
+
     return (
-        !posts.length ? <CircularProgress /> : (
+        !sortedPosts.length ? <CircularProgress /> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-                {posts.map((post) => (
+                {sortedPosts.map((post) => (
                     <Grid key={post._id} item xs={12} sm={6}>
                         <Post post={post} setCurrentId={setCurrentId} />
                     </Grid>
@@ -23,3 +33,33 @@ const Posts = ({ setCurrentId }) => {
 }
 
 export default Posts;
+
+
+// import React from "react";
+// import { Grid, CircularProgress } from "@material-ui/core";
+// import { useSelector } from "react-redux";
+
+// import Post from "./Post/Post";
+// import useStyles from './styles';
+
+// const Posts = ({ setCurrentId }) => {
+//     const posts = useSelector((state) => state.posts);
+//     const classes = useStyles();
+
+//     // Sort the posts array in descending order by createdAt
+//     const sortedPosts = [...posts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+//     return (
+//         !sortedPosts.length ? <CircularProgress /> : (
+//             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+//                 {sortedPosts.map((post) => (
+//                     <Grid key={post._id} item xs={12} sm={6}>
+//                         <Post post={post} setCurrentId={setCurrentId} />
+//                     </Grid>
+//                 ))}
+//             </Grid>
+//         )
+//     );
+// }
+
+// export default Posts;
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
   title: String,
@@ -17,10 +17,37 @@ const postSchema = mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: new Date(),
+    default: Date.now, // This will store the timestamp in ISO 8601 format
   },
 });
 
 const PostMessage = mongoose.model("PostMessage", postSchema);
 
 export default PostMessage;
+
+// import mongoose from "mongoose";
+
+// const postSchema = mongoose.Schema({
+//   title: String,
+//   message: String,
+//   creator: String,
+//   tags: [String],
+//   selectedFile: String,
+//   comments: [String],
+//   likeCount: {
+//     type: Number,
+//     default: 0,
+//   },
+//   dislikeCount: {
+//     type: Number,
+//     default: 0,
+//   },
+//   createdAt: {
+//     type: String, // Change the type to String
+//     default: new Date().toISOString(), // Use new Date().toISOString() to set the default value
+//   },
+// });
+
+// const PostMessage = mongoose.model("PostMessage", postSchema);
+
+// export default PostMessage;
